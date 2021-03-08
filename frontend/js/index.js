e453fe4f0b9b9a551ff853d1cb953f790f3c131b
Cloudymolecule <baires82@gmail.com>
2021-03-08 12:23:50 -0500

    Modify user form and css

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const f = document.createElement('form')
         f.setAttribute('method', 'post')
-        f.setAttribute('action', 'submit')
-        f.setAttribute('class', 'interface')
+        f.setAttribute('action', 'http://localhost:3000')
+        f.setAttribute('class', 'form')
         
         const p = document.createElement('p')
         p.innerText = "Create a ScapeX account"
@@ -38,36 +38,67 @@ document.addEventListener("DOMContentLoaded", () => {
         labName.setAttribute('type', 'label')
         labName.setAttribute('class', 'input-styles')
         labName.setAttribute('for', 'input-name')
-        labName.innerText = "Please enter your name:"
+        labName.innerText = "Username:"
 
         const inputName = document.createElement('input')
         inputName.setAttribute('type', 'text')
         inputName.setAttribute('name', 'username')
-        inputName.setAttribute('class', 'input-styles')
+        inputName.setAttribute('class', 'input-styles-inp')
         inputName.setAttribute('id', 'input-name')
 
         const labEmail = document.createElement('label')
         labEmail.setAttribute('type', 'label')
         labEmail.setAttribute('class', 'input-styles')
         labEmail.setAttribute('for', 'input-email')
-        labEmail.innerText = "Please enter your email:"
+        labEmail.innerText = "Email:"
 
         const inputEmail = document.createElement('input')
         inputEmail.setAttribute('type', 'text')
         inputEmail.setAttribute('name', 'username')
-        inputEmail.setAttribute('class', 'input-styles')
+        inputEmail.setAttribute('class', 'input-styles-inp')
         inputEmail.setAttribute('id', 'input-email')
+
+        const labPass = document.createElement('label')
+        labPass.setAttribute('type', 'label')
+        labPass.setAttribute('class', 'input-styles')
+        labPass.setAttribute('for', 'input-password')
+        labPass.innerText = "Password:"
+
+        const inputPass = document.createElement('input')
+        inputPass.setAttribute('type', 'password')
+        inputPass.setAttribute('password', 'password')
+        inputPass.setAttribute('class', 'input-styles-inp')
+        inputPass.setAttribute('id', 'input-password')
+
+        const labPassCon = document.createElement('label')
+        labPassCon.setAttribute('type', 'label')
+        labPassCon.setAttribute('class', 'input-styles')
+        labPassCon.setAttribute('for', 'input-password-con')
+        labPassCon.innerText = "Confirm password:"
+
+        const inputPassCon = document.createElement('input')
+        inputPassCon.setAttribute('type', 'password')
+        inputPassCon.setAttribute('password-con', 'password-con')
+        inputPassCon.setAttribute('class', 'input-styles-inp')
+        inputPassCon.setAttribute('id', 'input-password-con')
+
+        const br = document.createElement('br')
 
         const s = document.createElement('input')
         s.setAttribute('type', 'submit')
         s.setAttribute('value', 'Register')
-        s.setAttribute('class', 'input-styles')
+        s.setAttribute('class', 'input-styles-button')
 
         interface.appendChild(p)
         f.appendChild(labName)
         f.appendChild(inputName)
         f.appendChild(labEmail)
         f.appendChild(inputEmail)
+        f.appendChild(labPass)
+        f.appendChild(inputPass)
+        f.appendChild(labPassCon)
+        f.appendChild(inputPassCon)
+        f.appendChild(br)
         f.appendChild(s)
 
 
