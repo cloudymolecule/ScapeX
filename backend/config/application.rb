02eef43cf35018185bafa64a5ff084490c5adfed
Cloudymolecule <baires82@gmail.com>
2021-03-08 12:33:16 -0500

    Add user resource

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -19,6 +19,16 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+      :headers => :any,
+      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+      :max_age => 0
+  end
+end 
+
 module ScapexApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
