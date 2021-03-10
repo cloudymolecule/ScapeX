6d7d2c8bd157a51124238066193ceaf195ace7d6
Cloudymolecule <baires82@gmail.com>
2021-03-09 22:46:55 -0500

    Add validation for username and email

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true
     validates :password, presence: true, confirmation: true, on: :create
 end
