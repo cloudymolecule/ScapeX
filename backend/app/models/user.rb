6b4b7ebe2f723c6443eee7fce87f3ff3e9096a1d
Cloudymolecule <baires82@gmail.com>
2021-03-11 22:19:40 -0500

    Add Room validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
-    validates :password, presence: true, confirmation: true, on: :create
+    validates :password, presence: true, confirmation: true
     validates :email, email: true
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :items
   resources :rooms
-  resources :users 
-  resources :sessions
-  # post '/users', to: 'users#create'
-  # post '/login', to: 'sessions#login'
-  # get '/logout', to: 'sessions#logout'
+  # resources :users 
+  # resources :sessions
+  post '/users', to: 'users#create'
+  post '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
