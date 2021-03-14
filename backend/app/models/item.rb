5de4e3121f7a8bec8eed6ffacfa2f80182f13d43
Cloudymolecule <baires82@gmail.com>
2021-03-14 19:08:05 -0400

    Update item validations

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -2,13 +2,15 @@ class Item < ApplicationRecord
     belongs_to :room
 
     validates :name, :description, :looked_message, presence: true
-    validates :take_message, presence: true, if: :take_validation
-   
     
+    validates :take_message, presence: true, if: :take_validation
     validates :closed_message, presence: true, if: :closed_validation
     validates :talk_message, presence: true, if: :talk_validation
     validates :locked_message, presence: true, if: :locked_validation
     validates :opened_message, presence: true, if: :locked_validation
+
+    validates :name, length: {maximum: 20}
+    validates :description, :looked_message, :closed_message, :talk_message, :locked_message, :opened_message, length: {maximum: 100}
 
     def take_validation
         take 
